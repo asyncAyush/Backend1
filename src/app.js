969710b20f04874acff59499022cf0ae43df351b
asyncAyush <ayushkumar905624681@gmail.com>
2025-08-21 23:13:45 +0530

    Frontend url

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // ✅ CORS Setup
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173", "https://frontend1-imx8.vercel.app/"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
