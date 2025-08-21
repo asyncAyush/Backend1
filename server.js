645fa3e873285a005eef788d36a8d1623a5b56df
asyncAyush <ayushkumar905624681@gmail.com>
2025-08-21 23:50:55 +0530

    path change

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
+const app = require('./src/app')
+const connectDB = require('./src/db/db')
 
-const app = require('../../src/app.js');
-const connectDB = require('../../src/db/db');
 require('dotenv').config()
-
 connectDB()
 app.listen(3000, async(req , res) => {
     console.log("server is running on port 3000")
